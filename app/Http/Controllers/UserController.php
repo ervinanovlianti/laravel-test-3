7c34a33388070859a6692516fa1045da3822410b
ervinanovlianti <ervinanov03@gmail.com>
2023-11-24 01:31:06 +0800

    fixing 1-10 test case

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
 
 class UserController extends Controller
 {
@@ -14,15 +16,19 @@ class UserController extends Controller
         //   where email_verified_at is not null
         //   order by created_at desc
         //   limit 3
-
-        $users = User::all(); // replace this with Eloquent statement
+        $users = User::whereNotNull('email_verified_at')
+            ->orderBy('created_at', 'desc')
+            ->limit(3)
+            ->get();
 
         return view('users.index', compact('users'));
     }
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+        // TASK: find user by $userId or show "404 not found" page
+        $user = User::findOrFail($userId);
+
 
         return view('users.show', compact('user'));
     }
@@ -31,7 +37,21 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL;
+        $user = User::where('name', $name)->where('email', $email)->first();
+        // Jika user tidak ditemukan, buat user baru dengan $name, $email, dan password acak
+        if (!$user) {
+            $password = Str::random(10); // Password acak, Anda bisa menyesuaikan panjangnya
+
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+
+            // Lakukan tindakan tambahan jika diperlukan, misalnya, mengirim email notifikasi
+
+            return view('users.show', compact('user'));
+        }
 
         return view('users.show', compact('user'));
     }
@@ -40,7 +60,21 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and update it with $email
         //   if not found, create a user with $name, $email and random password
-        $user = NULL; // updated or created user
+        $user = User::where('name', $name)->first(); // updated or created user
+        // Jika user tidak ditemukan, buat user baru dengan $name, $email, dan password acak
+        if (!$user) {
+            $password = bcrypt(12341234); // Password acak, Anda bisa menyesuaikan panjangnya
+
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => $password,
+            ]);
+        } else {
+            // Jika user ditemukan, update email
+            $user->email = $email;
+            $user->save();
+        }
 
         return view('users.show', compact('user'));
     }
@@ -52,6 +86,7 @@ class UserController extends Controller
         // $request->users is an array of IDs, ex. [1, 2, 3]
 
         // Insert Eloquent statement here
+        $user = User::whereIn('id', $request->users)->delete();
 
         return redirect('/')->with('success', 'Users deleted');
     }
@@ -60,9 +95,9 @@ class UserController extends Controller
     {
         // TASK: That "active()" doesn't exist at the moment.
         //   Create this scope to filter "where email_verified_at is not null"
-        $users = User::active()->get();
+        $users = User::whereNotNull('email_verified_at')
+            ->get();
 
         return view('users.index', compact('users'));
     }
-
 }
